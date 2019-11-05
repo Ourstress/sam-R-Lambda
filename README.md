5d91d59be3ae59502fb93e9fa71e72cf0fb4f6b7
SG <sgng1@e.ntu.edu.sg>
2019-11-05 01:40:10 +0000

    remove some files

--- README.md
+++
@@ -1,1 +0,0 @@
-# sam-R-Lambda
--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 #sam build
 sam package --output-template \
-    packaged.yaml --s3-bucket samtestaishu
+    packaged.yaml --s3-bucket hello888
 sam  deploy --template-file packaged.yaml \
     --region us-east-1 --capabilities \
     CAPABILITY_IAM --stack-name samR
